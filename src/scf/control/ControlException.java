bb007ee4c00707e2ad4e4ae7a2c20bbf4bdcebe7
Wilson08 <wilson.ortega08@gmail.com>
2019-06-09 02:27:00 -0300

    add novos arquivos

---
+++ src/scf/control/ControlException.java
@@ -0,0 +1,23 @@
+package scf.control;
+
+public class ControlException extends Exception {
+	public ControlException() {
+		super();
+	}
+
+	public ControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ControlException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ControlException(String message) {
+		super(message);
+	}
+
+	public ControlException(Throwable cause) {
+		super(cause);
+	}
+}
